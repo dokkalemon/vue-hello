76c3e5de3f6725ddbc3e32ab31c9c804a280db87
dokkalemon <dottori.roberto@gmail..com>
2021-11-11 15:02:52 +0100

    end

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* 
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data di Vue.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+*/
+
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Ciao Ragazzi',
+        image: 'https://picsum.photos/200/300'
+
+    }
+})
